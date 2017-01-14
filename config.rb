fbf0dbd8512acb8b337bf537719f0aec18bdc77f
David Thomas <me@dt.id.au>
2017-01-14 23:48:05 +0800

    staging deploy

--- config.rb
+++ config.rb
@@ -46,3 +46,9 @@ configure :production do
   activate :minify_javascript
   activate :asset_host, :host => '//d2jh1s85ct6v2r.cloudfront.net'
 end
+
+configure :staging do
+  activate :minify_html
+  activate :minify_css
+  activate :minify_javascript
+end
