932bf97b3f64074cd30f67b66076e79c56146800
Teegan Lincoln <tlincoln@outlook.com>
2017-01-19 00:21:58 +0800

    add link, list, blockquote and header styles

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,14 @@
 // This is where it all goes :)
+$(window).scroll(function(){
+	$introHeight = $('header').height() * 2;
+	$scrolled = $(window).scrollTop();
+
+	//$("header .logo").css("opacity", 1 - $scrolled / $introHeight);
+	$("header .logo").css("top", -($scrolled * 0.25));
+
+	if ($scrolled >= $introHeight) {
+		//$('#header').addClass('sm-nav');
+	} else if ($scrolled < $introHeight) {
+		//$('#header').removeClass('sm-nav');
+	}
+});
