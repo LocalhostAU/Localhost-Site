509ba8338e608969492bbc99181271c403508120
David Thomas <david@thedavidthomas.com>
2017-03-25 21:49:04 +0800

    change to live sheet

--- source/javascripts/events.js
+++ source/javascripts/events.js
@@ -117,7 +117,11 @@ $(function() {
     });
 });
 
-var spreadsheetID = "1bsJUvCxn7LfBvLuktxhJzd96cA8sY8iRLbl9KE6hwzI";
+// Live
+ var spreadsheetID = "1tTuj1ptQhBimlp5p3AKyBtPavAKofGxn-3yMhhytv9E";
+// Dev
+ //var spreadsheetID = "1bsJUvCxn7LfBvLuktxhJzd96cA8sY8iRLbl9KE6hwzI";
+
 
 
 // Make sure it is public or set to Anyone with link can view
