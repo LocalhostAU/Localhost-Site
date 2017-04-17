7bceb8873944adeda86c8db0a84a0a09a00a3ee0
Teegan Lincoln <tlincoln@outlook.com>
2017-04-17 13:14:33 +0800

    Teegan: homepage updates, nav, upcoming events, future events calendar,blog/subscription section

--- source/javascripts/home.js
+++ source/javascripts/home.js
@@ -11,10 +11,10 @@ $.getJSON(featuredurl, function(data) {
   var featuredentry = data.feed.entry;
   $(featuredentry).each(function(i){
     if( i == 0) {
-      $('.localhost-upcoming-event').append('<p class="date"><span class="weekday">'+this.gsx$day.$t+' '+this.gsx$date.$t+'</span><span class="month">'+this.gsx$month.$t+'</span></p>');
+      $('.localhost-upcoming-event > .container-fluid').append('<p class="date"><span class="weekday">'+this.gsx$day.$t+' '+this.gsx$date.$t+'</span><span class="month">'+this.gsx$month.$t+'</span></p>');
     }
 
-    $('.localhost-upcoming-event').append('<div class="localhost-event"><h2 class="title">  <a href="'+this.gsx$link.$t+'" title="Find out about '+this.gsx$title.$t+'" target="_blank"><span data-hover="'+this.gsx$title.$t+'">'+this.gsx$title.$t+'</span></a></h2><p class="speaker">'+this.gsx$speaker.$t+' <span class="from">'+this.gsx$speakerfrom.$t+'</span></p> <p class="location">'+this.gsx$location.$t+'</p></div>');
+    $('.localhost-upcoming-event > .container-fluid').append('<div class="localhost-event"><h2 class="title">  <a href="'+this.gsx$link.$t+'" title="Find out about '+this.gsx$title.$t+'" target="_blank"><span data-hover="'+this.gsx$title.$t+'">'+this.gsx$title.$t+'</span></a></h2><p class="speaker">'+this.gsx$speaker.$t+' <span class="from">'+this.gsx$speakerfrom.$t+'</span></p> <p class="location"><span>'+this.gsx$location.$t+'</span></p></div>');
   });
 });
 
@@ -57,7 +57,7 @@ $.getJSON(url, function(data) {
         if(i>10) return false;
         var month = this.gsx$month.$t;
         if(month != newmonth | i == 0) {
-          $('.future-events ul').append('<li class="date-title"><p class="date month">'+moment(this.gsx$startdate.$t+"-"+this.gsx$month.$t+"-"+this.gsx$year.$t, "DD-MM-YYYY").format("MMM")+'</p></li>');
+          $('.future-events ul').append('<li class="date-title"><p class="date month">'+moment(this.gsx$startdate.$t+"-"+this.gsx$month.$t+"-"+this.gsx$year.$t, "DD-MM-YYYY").format("MMMM")+'</p></li>');
         }
 
         var $locationEvent = this.gsx$location.$t.toLowerCase();
@@ -75,7 +75,7 @@ $.getJSON(url, function(data) {
       $('.future-events ul').append('<li class="'+$locationEvent+'-event '+statusClass+' '+featuredClass+'">\
         <a href="'+this.gsx$link.$t+'" class="title" target="_blank">\
           <span class="calendar-event-date calendar-past-date">'+ moment(this.gsx$startdate.$t+"-"+this.gsx$month.$t+"-"+this.gsx$year.$t, "DD-MM-YYYY").format("ddd DD")+endDate+'</span>\
-          <span class="title">'+this.gsx$title.$t+' <span>'+this.gsx$type.$t+'</span><span class="location">'+this.gsx$location.$t+'</span></span>\
+          <span class="title"><span class="event-title">'+this.gsx$title.$t+'</span><span class="event-type">'+this.gsx$type.$t+'</span><span class="location">'+this.gsx$location.$t+'</span></span>\
         </a>\
       </li>');
       newmonth = this.gsx$month.$t;
