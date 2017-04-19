5f6d99f3e26dd83f609f2fb316fdc1ad60425383
David Thomas <david@thedavidthomas.com>
2017-04-19 20:40:06 +0800

    remove some test files add redirects and create blank deployment page

--- config.rb
+++ config.rb
@@ -15,6 +15,7 @@ page '/*.txt', layout: false
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
 #  which_fake_page: "Rendering a fake page with a local variable" }
+proxy "_redirects", "netlify-redirects", ignore: true
 
 # General configuration
 
